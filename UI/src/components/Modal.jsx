501412198dceb5eb0250706cf6a9085b2668641c
蔡荣诠 <cairq@tongbaninfo.com>
2023-09-11 17:42:28 +0800

    20230911

---
+++ UI/src/components/Modal.jsx
@@ -0,0 +1,7 @@
+export default function Modal(props) {
+  return (
+    <div className="modal-view">
+      <div className="modal-mask-view"></div>
+    </div>
+  )
+}
--- UI/src/views/ProjectManage/index.jsx
+++ UI/src/views/ProjectManage/index.jsx
@@ -1,9 +1,16 @@
 import './index.scss'
+import Button from '../../components/Button'
+import { PackagePlus } from 'lucide-react'
 
 export default function projectManage() {
   return (
     <div className="project-manage">
-      1
+      <header>
+        <Button>
+          <PackagePlus size={18}></PackagePlus>
+          <span className='ml-[6px]'>new project</span>
+        </Button>
+      </header>
     </div>
   )
 }
